68d7e35635ecef1e2f097350677a52624c2a2e3a
seryozha59 <120307628+seryozha59@users.noreply.github.com>
2025-05-17 20:53:18 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ win_height = 500
 window = display.set_mode((700, 500))
 window.fill(background)
 
-playerL = transform.scale(image.load('playerL.jpeg'), (25, 100))
+playerL = transform.scale(image.load('playerL.jpeg'), (35, 200))
+playerR = transform.scale(image.load('playerR.jpeg'), (35, 200))
+ball = transform.scale(image.load('ball.png'), (65, 65))
 
 font.init()
 font = font.SysFont('Arial', 70)
@@ -19,7 +21,7 @@ finish = False
 class GameSprite(sprite.Sprite):
    def __init__(self, player_image, player_x, player_y, player_speed):
        super().__init__()
-       self.image = transform.scale(image.load(player_image), (65, 65))
+       self.image = transform.scale(image.load(player_image), (35, 200))
        self.speed = player_speed
        self.rect = self.image.get_rect()
        self.rect.x = player_x
@@ -32,17 +34,17 @@ class Player(GameSprite):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys_pressed[K_s] and self.rect.y < 500 - 95:
+        if keys_pressed[K_s] and self.rect.y < 500 - 205:
             self.rect.y += self.speed
     def updateR(self):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys_pressed[K_DOWN] and self.rect.y < 500 - 95:
+        if keys_pressed[K_DOWN] and self.rect.y < 500 - 205:
             self.rect.y += self.speed
 
-playerL = Player(playerL, 25, 150, 3)
-playerR = Player('playerR.jpeg', 675, 150, 3)
+playerL = Player('playerL.jpeg', 20, 150, 3)
+playerR = Player('playerR.jpeg', 650, 150, 3)
 
 while game:
     if finish != True:
